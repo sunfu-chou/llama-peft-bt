b6405d2e80f1daf7ed4ab2098a42ad3fa84af8fc
sunfu-chou <sunfu.chou@gmail.com>
2024-06-18 21:57:13 +0800

    Update push_dataset_to_hub

--- push_dataset_to_hub.py
+++ push_dataset_to_hub.py
@@ -1,7 +1,14 @@
-from datasets import load_dataset
+from datasets import DatasetDict, load_dataset
 
-with open("data/dataset.json", "r", encoding="utf-8") as file:
-    print(file.read())
+# with open("data/dataset.json", "r", encoding="utf-8") as file:
+#     print(file.read())
 
-dataset = load_dataset("json", data_files="data/dataset.json")
+train_dataset = load_dataset("json", data_files="data/dataset.json")
+test_dataset = load_dataset("json", data_files="data/object_navigation/dataset.json")
+
+dataset = DatasetDict({
+    "train": train_dataset["train"],
+    "test": test_dataset["train"]
+})
+
 dataset.push_to_hub("sunfu-chou/symbolic-bt")
