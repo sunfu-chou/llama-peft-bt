c55c2995072c1a0660b686ae28648115aea65b9d
sunfu-chou <sunfu.chou@gmail.com>
2024-06-20 10:43:04 +0800

    Update push_dataset_to_hub.py

--- push_dataset_to_hub.py
+++ push_dataset_to_hub.py
@@ -1,8 +1,5 @@
 from datasets import DatasetDict, load_dataset
 
-# with open("data/dataset.json", "r", encoding="utf-8") as file:
-#     print(file.read())
-
 train_dataset = load_dataset("json", data_files="data/dataset.json")
 test_dataset = load_dataset("json", data_files="data/object_navigation/dataset.json")
 
